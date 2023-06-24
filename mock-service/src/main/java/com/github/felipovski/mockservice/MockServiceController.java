4b4532af5a2f04033338c7af5df10e596f93a261
Felipe <felipovski@gmail.com>
2023-06-24 00:16:38 -0300

    First commit

---
+++ mock-service/src/main/java/com/github/felipovski/mockservice/MockServiceController.java
@@ -0,0 +1,17 @@
+package com.github.felipovski.mockservice;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "*")
+public class MockServiceController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
---
+++ mock-service/src/main/java/com/github/felipovski/mockservice/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.github.felipovski.mockservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Value("${jwt-uri}")
+    private String jwtUri;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
+                .formLogin().disable();
+        return http.build();
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwtUri).build();
+    }
+
+}
